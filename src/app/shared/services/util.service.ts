0407159f1b7483456bb269d5692b947e8b79bb73
Durga Panda <durgaprasadpan@gmail.com>
2019-01-13 19:05:12 +0530

    attendance and raise a ticket updated

--- src/app/shared/services/util.service.ts
+++ src/app/shared/services/util.service.ts
@@ -51,4 +51,23 @@ export class UtilService {
 
     return `${now.getDate()}-${months[now.getMonth()]}-${now.getFullYear()}`;
   }
+
+  compareDates(date) {
+    // date - 'yyyy-mm-dd'
+    const now = new Date();
+
+    let dayofMonth = now.getDate();
+    dayofMonth = dayofMonth < 10 ? `0${dayofMonth}` : `${dayofMonth}`;
+
+    let month = now.getMonth() + 1;
+    month = month < 10 ? `0${month}` : `${month}`;
+
+    const formattedDate = `${now.getFullYear()}-${month}-${dayofMonth}`;
+
+    if (date === formattedDate) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
